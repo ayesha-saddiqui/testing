a6f5d6789ffd9719a77a8ba6fe3c874f3799abd7
Ashoo <iayeshasaddique@gmail.com>
2024-08-13 17:48:11 +0500

    change css file import in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Analytic from "./components/Analytic";
 import Newsletter from "./components/Newsletter";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
+import "./index.css";
 
 function App() {
   return (
